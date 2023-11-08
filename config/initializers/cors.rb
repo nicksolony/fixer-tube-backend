9e4571e36c119e707bd5fb48c637e5eb0d283a19
nicksolony <nicksolony@gmail.com>
2023-11-07 21:40:54 -0500

    updating heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,6 @@
 
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-# updating heroku stack
 
 
 # Read more: https://github.com/cyu/rack-cors
